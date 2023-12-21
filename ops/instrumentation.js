d61a6f975fe4bb56bedd59408d4d814a0b89bb7c
Leandro <leandro@MBL-KQ9HFXP0M2.local>
2023-12-21 13:03:39 -0300

    Add Zipkin

--- ops/instrumentation.js
+++ ops/instrumentation.js
@@ -1,28 +1,32 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const { getNodeAutoInstrumentations, } = require('@opentelemetry/auto-instrumentations-node');
-const { PeriodicExportingMetricReader, ConsoleMetricExporter, } = require('@opentelemetry/sdk-metrics');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
 const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes, } = require('@opentelemetry/semantic-conventions');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const opentelemetry = require('@opentelemetry/api');
 
-
 const tracer = opentelemetry.trace.getTracer(
-    'instrumentation-scope-name',
-    'instrumentation-scope-version',
-  );
+  'instrumentation-scope-name',
+  'instrumentation-scope-version',
+);
 
+// Configurar o exportador do Zipkin
+const zipkinExporter = new ZipkinExporter({
+  serviceName: 'SERVER-API-LOGIN',
+  url: 'http://localhost:9411/api/v2/spans', // Substitua pela URL do seu servidor Zipkin
+});
 
 const sdk = new NodeSDK({
-    resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: 'SERVER-API-LOGIN',
-        [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
-    }),
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
-    instrumentations: [getNodeAutoInstrumentations()],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'SERVER-API-LOGIN',
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  }),
+  traceExporter: zipkinExporter, // Usar o exportador do Zipkin
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start();
