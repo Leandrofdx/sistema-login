336e76420dd82f072ae5fad7613b28f71ff28a16
Leandro <leandro@MBL-KQ9HFXP0M2.local>
2023-12-21 20:42:10 -0300

    Ajustar ordenação

--- app.js
+++ app.js
@@ -6,32 +6,23 @@ const userRoutes = require('./src/routes/userRoutes');
 const rateLimit = require('express-rate-limit');
 const fs = require('fs');
 const path = require('path');
-const http2Express = require('http2-express-bridge')
-const http2 = require('http2')
+const http2Express = require('http2-express-bridge');
+const http2 = require('http2');
 
+// Configuração do servidor HTTP/2
 const options = {
     key: fs.readFileSync(path.resolve('./certs/server.key')),
     cert: fs.readFileSync(path.resolve('./certs/server.crt')),
     allowHTTP1: true,
 };
 
-//const app = express();
-const app = http2Express(express)
+const app = http2Express(express);
 const port = 3000;
+const server = http2.createSecureServer(options, app);
 
+// Middlewares e configurações globais
 app.use(bodyParser.json());
-
-// Configuração do rate limit (10 requisições por segundo)
-const limiter = rateLimit({
-    windowMs: 1000, // 1 segundo
-    max: 10000, // 10 requisições por segundo
-    message: 'Limite de requisições excedido. Tente novamente mais tarde.',
-});
-
-// Aplica o rate limit a todas as rotas
-app.use(limiter);
-
-// Rota de autenticação
+app.use(rateLimit({ windowMs: 1000, max: 10000, message: 'Limite de requisições excedido. Tente novamente mais tarde.' }));
 app.use(authRoutes);
 
 // Middleware para imprimir a versão do protocolo
@@ -40,13 +31,13 @@ app.use('/', (req, res, next) => {
     next();
 });
 
-// Configuração para servir arquivos estáticos da pasta 'public'
-app.use('/' , express.static(path.join(__dirname, 'public')));
+// Servir arquivos estáticos da pasta 'public'
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Rotas protegidas com autenticação JWT (deve vir após as rotas de autenticação)
 app.use(authenticateToken);
 
-// Rota de boas-vindas
+// Rotas
 app.get('/', (req, res) => {
     res.send('Bem-vindo');
     console.log(`Versão do Protocolo HTTP: ${req.httpVersion}`);
@@ -55,7 +46,7 @@ app.get('/', (req, res) => {
 // Rota de cadastro de usuário (sem autenticação)
 app.use('/usuarios', userRoutes);
 
-// Rotas retorno usuários (protegida com autenticação JWT)
+// Rotas de retorno de usuários (protegidas com autenticação JWT)
 app.use('/usuarios', authenticateToken, userRoutes);
 
 // Tratamento de erro para rotas inexistentes
@@ -69,10 +60,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Algo deu errado!');
 });
 
-const server = http2.createSecureServer(options, app)
-
-app.server = server;
-
+// Iniciar o servidor
 server.listen(port, () => {
     console.log(`Servidor rodando em https://localhost:${port}`);
 });
