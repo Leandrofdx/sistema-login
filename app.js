901a8f686da75f05121dd32b1fbd15ec608d83ec
Leandro <leandro@MBL-KQ9HFXP0M2.local>
2023-12-22 14:28:19 -0300

    Atualizacao

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const http2 = require('http2');
+const http2Express = require('http2-express-bridge');
+const path = require('path');
+const rateLimit = require('express-rate-limit');
+const glob = require('glob');
+const mime = require('mime');
+const compression = require('compression');
+const sharp = require('sharp');
+
+// Importar módulos locais
 const { authenticateToken } = require('./src/middlewares/authMiddleware');
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
-const rateLimit = require('express-rate-limit');
-const fs = require('fs');
-const path = require('path');
-const http2Express = require('http2-express-bridge');
-const http2 = require('http2');
 
 // Configuração do servidor HTTP/2
 const options = {
@@ -20,50 +26,55 @@ const app = http2Express(express);
 const port = 3000;
 const server = http2.createSecureServer(options, app);
 
+// Middleware global para imprimir a versão do protocolo
+app.use((req, res, next) => {
+    console.log(`Versão do Protocolo HTTP: ${req.httpVersion}`);
+    next();
+});
+
 // Middlewares e configurações globais
 app.use(bodyParser.json());
 app.use(rateLimit({ windowMs: 1000, max: 10000, message: 'Limite de requisições excedido. Tente novamente mais tarde.' }));
-app.use(authRoutes);
 
+// Compressão de resposta (com verificação para HTTP/2)
 app.use((req, res, next) => {
-    console.log(`Versão do Protocolo HTTP: ${req.httpVersion}`);
-    next();
+    if (req.httpVersion !== '2.0') {
+        compression()(req, res, next);
+    } else {
+        next();
+    }
 });
 
 // Servir arquivos estáticos da pasta 'public' usando HTTP/2
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31536000 })); // Cache de recursos estáticos por 1 ano
 
-// Middleware para imprimir a versão do protocolo
-app.use((req, res, next) => {
-    console.log(`Versão do Protocolo HTTP: ${req.httpVersion}`);
-    next();
-});
+app.use(authRoutes);
 
 // Rotas protegidas com autenticação JWT (deve vir após as rotas de autenticação)
 app.use(authenticateToken);
 
 // Rota de boas-vindas
 app.get('/', (req, res) => {
+    pushStaticResourcesDynamic(res, 'styles'); // Empurrar arquivos CSS
+    pushStaticResourcesDynamic(res, 'scripts'); // Empurrar arquivos JavaScript
     res.send('Bem-vindo');
-    console.log(`Versão do Protocolo HTTP: ${req.httpVersion}`);
 });
 
-// Rota de cadastro de usuário (sem autenticação)
+// Rotas de usuário (protegidas com autenticação JWT)
 app.use('/usuarios', userRoutes);
 
-// Rotas de retorno de usuários (protegidas com autenticação JWT)
-app.use('/usuarios', authenticateToken, userRoutes);
-
-// Tratamento de erro para rotas inexistentes
-app.use((req, res) => {
-    res.status(404).send('Rota não encontrada!');
-});
-
-// Middleware de tratamento de erros
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Algo deu errado!');
-});
+// Função para empurrar arquivos estáticos de um diretório
+function pushStaticResourcesDynamic(res, folder) {
+    const files = glob.sync(`./public/${folder}/*.*`);
+    files.forEach(file => {
+        const relativePath = path.relative(path.join(__dirname, 'public'), file);
+        const contentType = mime.getType(file) || 'application/octet-stream';
+        res.push(`/${relativePath}`, {
+            request: { accept: '*/*' },
+            response: { 'content-type': contentType },
+        }).end(fs.readFileSync(file));
+    });
+}
 
 // Iniciar o servidor
 server.listen(port, () => {
